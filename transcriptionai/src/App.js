ce2192da62f172e550939855ba346915d1504bc3
NotCr4fter <chaos11adam@gmail.com>
2024-06-23 16:09:34 +0800

    Built a basic react app for my Video Transcription feature.
    Having errors with the processing part. Ill update once its good to go

---
+++ transcriptionai/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import VideoTranscription from './VideoTranscription';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <VideoTranscription />
+      </header>
+    </div>
+  );
+}
+
+export default App;
